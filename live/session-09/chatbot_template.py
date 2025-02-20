011c7e4d783972b95c48197ba784db6fe38ce9be
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2025-02-20 19:49:08 +0900

    Add

---
+++ live/session-09/chatbot_template.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("⚽️ TEST")
+
+with st.sidebar:
+    openai_api_key = st.text_input(
+        "OpenAI API Key",
+        type="password",
+        value=st.secrets["openai_api_key"],
+    )
+
+if prompt := st.chat_input("무엇이 궁금하세요?"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        st.write("저는 완성되지 않은 Chatbot이라 잘 모르겠습니다ㅜㅜ")
